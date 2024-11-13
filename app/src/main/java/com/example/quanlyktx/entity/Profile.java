6acd1face7bb233b33aa33fde4e090298feb5f52
Vu Nguyen Tien <gtvvunguye@gmail.com>
2024-11-13 22:41:19 +0700

    create some base entities of project

---
+++ app/src/main/java/com/example/quanlyktx/entity/Profile.java
@@ -0,0 +1,44 @@
+package com.example.quanlyktx.entity;
+
+public class Profile {
+    String username, sdt, email, name;
+
+    public Profile(String username, String sdt, String email, String name) {
+        this.username = username;
+        this.sdt = sdt;
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
